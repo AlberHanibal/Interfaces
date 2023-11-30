f02f1ad351e232c4f5a6d002964295fbb1c81565
Alber clase <alber@jovellanos>
2023-11-30 14:21:23 +0100

    organizacion

---
+++ di-015/di-015/src/main/java/com/example/Controlador.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class Controlador{
+
+    @FXML
+    private void manejadorEvento() throws IOException {
+            System.out.println("Esto es un manejador");
+    }
+}
